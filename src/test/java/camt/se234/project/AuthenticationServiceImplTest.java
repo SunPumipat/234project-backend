8df84ea100d5ebf7573e6cbeec307d06662d3aa0
sun_pumipat <pumipat.tip@gmail.com>
2018-04-28 22:47:51 +0700

    create mathod: authenticateTest

--- src/test/java/camt/se234/project/AuthenticationServiceImplTest.java
+++ src/test/java/camt/se234/project/AuthenticationServiceImplTest.java
@@ -5,7 +5,9 @@ import camt.se234.project.service.AuthenticationServiceImpl;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.hamcrest.Matchers.is;
 import static org.mockito.Mockito.mock;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class AuthenticationServiceImplTest {
     UserDao userDao;
@@ -18,5 +20,9 @@ public class AuthenticationServiceImplTest {
         authenticationService.setUserDao(userDao);
     }
 
-    
+    @Test
+    public void authenticateTest(){
+        assertThat(authenticationService.authenticate("sun","1234"),is(userDao.getUser("sun", "1234")));
+        assertThat(authenticationService.authenticate("dan","1234"),is(userDao.getUser("dan", "1234")));
+    }
 }
