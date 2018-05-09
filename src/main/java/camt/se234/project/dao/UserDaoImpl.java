14265a936f5664b366f965166b6bfb3b38de9c96
sun_pumipat <pumipat.tip@gmail.com>
2018-05-09 14:15:15 +0700

    edit java

--- src/main/java/camt/se234/project/dao/UserDaoImpl.java
+++ src/main/java/camt/se234/project/dao/UserDaoImpl.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class UserDaoImpl implements UserDao{
+public class  UserDaoImpl implements UserDao{
     @Autowired
     UserRepository userRepository;
     @Override
